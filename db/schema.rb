aedaee9b9b2153584fd44e3ea57764394fd5750c
Sam Brannon <sam@branberg.com>
2014-10-26 11:11:07 -0500

    Added colors for social network fields

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20141026011043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "band_styles", force: true do |t|
-    t.integer  "band_id"
-    t.string   "field_name"
-    t.string   "field_value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "band_users", force: true do |t|
     t.integer  "user_id"
     t.integer  "band_id"
