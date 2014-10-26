ec5df23b697c11bf019aa3e9c54bf0058cea33dc
Sam Brannon <sam@branberg.com>
2014-10-25 22:01:11 -0500

    Added some jquery toggles

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -3,6 +3,21 @@
 //= require jquery
 jQuery(document).ready(function($) {
 
+  //toggle menu states
+  $("#menu_sections button.menu_section_label").on('click', function(){
+
+    if( $(this).parents('li').hasClass('active') ){
+      //close it
+      $(this).siblings('.menu_section').slideUp(200);
+      $(this).parents('li').removeClass('active');
+    } else {
+      $(this).siblings('.menu_section').slideDown(200);
+      $(this).parents('li').addClass('active');
+    }
+
+  });
+
+  //load up custom select fields
   $('select').customSelect();
 
 });
