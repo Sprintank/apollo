8027ffa75066305239cedc1187615ce1caaab3f3
alutz <adam.lutz@gokartlabs.com>
2014-11-22 15:35:16 -0600

    adding opacity slider w/ hidden field

--- app/controllers/admin/bands_controller.rb
+++ app/controllers/admin/bands_controller.rb
@@ -32,6 +32,7 @@ class Admin::BandsController < Admin::BaseController
     @music_background_color_field_option = FieldOption.find_by(:identifier => 'music_background_color')
     @background_image_field_option = FieldOption.find_by(:identifier => 'background_image')
     @background_overlay_color_field_option = FieldOption.find_by(:identifier => 'background_overlay_color')
+    @background_overlay_opacity_field_option = FieldOption.find_by(:identifier => 'background_overlay_opacity')
     @social_field_options = FieldOption.social.all
   end
 
