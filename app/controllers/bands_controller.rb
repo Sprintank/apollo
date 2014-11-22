aa3315acf748aa09970202dfe7b31367c38686f3
alutz <adam.lutz@gokartlabs.com>
2014-11-22 15:40:41 -0600

    loading all field options for public band page

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -13,9 +13,15 @@ class BandsController < ApplicationController
 
   def load_field_options
     @main_color_field_option = FieldOption.find_by(:identifier => 'main_color')
+    @main_color_font_option  = FieldOption.find_by(:identifier => 'main_font')
+
     @text_color_field_option = FieldOption.find_by(:identifier => 'text_color')
+    @text_color_font_option  = FieldOption.find_by(:identifier => 'text_font')
+
     @music_background_color_field_option = FieldOption.find_by(:identifier => 'music_background_color')
+    @background_image_field_option = FieldOption.find_by(:identifier => 'background_image')
     @background_overlay_color_field_option = FieldOption.find_by(:identifier => 'background_overlay_color')
+    @background_overlay_opacity_field_option = FieldOption.find_by(:identifier => 'background_overlay_opacity')
     @social_field_options = FieldOption.social.all
   end
 
