039de07db20e4c775d2e9b8dd08eed3fa7086d31
alutz <adam.lutz@gokartlabs.com>
2014-11-22 12:11:55 -0600

    adding theme.js to assets pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( admin/admin.css )
 Rails.application.config.assets.precompile += %w( themes/*/main.css )
+Rails.application.config.assets.precompile += %w( themes/*/theme.js )
