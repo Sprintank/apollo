aee2683ad4fc87675b0b482cdecdde0d7e8382ca
alutz <adam.lutz@gokartlabs.com>
2014-11-22 11:36:10 -0600

    adding themes directory to asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,2 @@
-# Rails.application.config.assets.precompile += %w( ie9.css )
 Rails.application.config.assets.precompile += %w( admin/admin.css )
-# Rails.application.config.assets.precompile += %w( pdf.css )
+Rails.application.config.assets.precompile += %w( themes/*/main.css )
