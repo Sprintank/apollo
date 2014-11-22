0c38889c26f0b5923ce1767ec7bec9bc20e62e55
alutz <adam.lutz@gokartlabs.com>
2014-11-22 15:14:34 -0600

    load field options helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def load_font_options
+    {
+      monsterrat: { :display_name => 'Monsterrat', :url => '' },
+            lato: { :display_name => 'Lato', :url => '' },
+      droid_sans: { :display_name => 'Droid Sans', :url => '' },
+    }
+  end
 end
